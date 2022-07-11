8e65e837ba40defc22741877703355d9617cecad
Marth1703 <marvin.fischer@hs-furtwangen.de>
2022-07-11 22:24:23 +0200

    First project setup

---
+++ TranslationMemory/TranslationApp/.vshistory/Progstart.cs/2022-07-11_22_22_58_000.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationApp
+{
+    internal class Progstart
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
